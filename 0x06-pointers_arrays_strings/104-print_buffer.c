3df9ab291a5be7b72245c8b15ce53c858727b771
MphoMalanga <clementinekhakhu@gmail.com>
2023-05-05 03:49:57 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,38 @@
-#include "main.h"
 #include <stdio.h>
-/**
-* print_buffer - prints buffer
-* @b: buffer
-* @size: size
-* Return: void
-*/
+#include <ctype.h>
+
 void print_buffer(char *b, int size)
 {
-int o, j, i;
-o = 0;
-if (size <= 0)
-{
-printf("\n");
-return;
-}
-while (o < size)
-{
-j = size - o < 10 ? size - o : 10;
-printf("%08x: ", o);
-for (i = 0; i < 10; i++)
-{
-if (i < j)
-printf("%02x", *(b + o + i));
-else
-printf(" ");
-if (i % 2)
-{
-printf(" ");
-}
-}
-for (i = 0; i < j; i++)
-{
-int c = *(b + o + i);
-if (c < 32 || c > 132)
-{
-c = '.';
-}
-printf("%c", c);
-}
-printf("\n");
-o += 10;
-}
+    int i, j;
+    char c;
+
+    if (size <= 0) {
+        printf("\n");
+        return;
+    }
+
+    for (i = 0; i < size; i += 10) {
+        printf("%08x: ", i);
+        for (j = i; j < i + 10; j++) {
+            if (j < size) {
+                printf("%02x ", (unsigned char)b[j]);
+            } else {
+                printf("   ");
+            }
+        }
+        printf(" ");
+        for (j = i; j < i + 10; j++) {
+            if (j < size) {
+                c = b[j];
+                if (isprint(c)) {
+                    printf("%c", c);
+                } else {
+                    printf(".");
+                }
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 }
