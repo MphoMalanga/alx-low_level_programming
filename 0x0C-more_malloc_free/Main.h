0a9a5380cb0e510353a2d14a3fef8dda08930b7c
Mpho Malanga <clementinekhakhu@gmail.com>
2023-06-07 18:02:20 +0100

    Updated

--- 0x0C-more_malloc_free/Main.h
+++ 0x0C-more_malloc_free/Main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+/**
+ * File: main.h
+ * Auth: Amos Mwongela
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -11,8 +14,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
 
 #endif
